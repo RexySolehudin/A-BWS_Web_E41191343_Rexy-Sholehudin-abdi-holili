d6f825abb8039a01cf71febc043842fd01f723d2
RexySolehudin <71806091+RexySolehudin@users.noreply.github.com>
2021-04-07 15:30:33 -0800

    tugas minggu3/3 membuat view dengan prameter

--- minggu3/app/Http/Controllers/ManagementUserController.php
+++ minggu3/app/Http/Controllers/ManagementUserController.php
@@ -8,7 +8,11 @@ class ManagementUserController extends Controller
 {
     //di sini isi controller management User
     public function index(){
-        return "Halo ini adalah method index,dalam controller ManagementUser";
+        //return "Halo ini adalah method index,dalam controller ManagementUser";
+        $nama = "Rexy Solehudin Abdi Holili";
+        $pelajaran = ["Algoritma dan pemugraman","Kalkulus","Pemograman web"];
+        return view('home', compact('nama','pelajaran'));
+        
     }
 
     public function create()
@@ -26,5 +30,13 @@ class ManagementUserController extends Controller
     public function edit($id)
     {
         return "Method ini nantinya akan digunakan untuk menampilkan form untuk mengubah data edit dengan id=" .$id;
+    }
+    public function update(Request $Request, $id)
+    {
+        return "Method ini nantinya akan digunakan untuk mengubah data user dengan id=" .$id;
+    }
+    public function destroy($id)
+    {
+        return "Method ini nantinya akan digunakan untuk menghapus data user dengan id=" .$id;
     }
 }
