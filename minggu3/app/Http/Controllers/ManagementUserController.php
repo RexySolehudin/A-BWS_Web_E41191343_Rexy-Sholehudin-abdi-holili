8cf02be6158c2ac13d347048d618f5d3edc3a10e
RexySolehudin <71806091+RexySolehudin@users.noreply.github.com>
2021-04-07 14:55:45 -0800

    Tugas minggu 3/2
    Mohon maaf keterlambatannya bapak/ibu dosen,karena file di repo sebelumnya lupa belum saya push sehingga file nya menghilang

---
+++ minggu3/app/Http/Controllers/ManagementUserController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ManagementUserController extends Controller
+{
+    //di sini isi controller management User
+    public function index(){
+        return "Halo ini adalah method index,dalam controller ManagementUser";
+    }
+
+    public function create()
+    {
+        return "Method ini nantinya akan digunakan untuk menampilkan form untuk menambah data user";
+    }
+    public function store(Request $Request)
+    {
+        return "Method ini nantinya akan digunakan untuk menciptakan data user yang baru";
+    }
+    public function show($id)
+    {
+        return "Method ini nantinya akan digunakan untuk mengambil satu data user dengan id=" .$id;
+    }
+    public function edit($id)
+    {
+        return "Method ini nantinya akan digunakan untuk menampilkan form untuk mengubah data edit dengan id=" .$id;
+    }
+}
